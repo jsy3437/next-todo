c4a58273a2edcdac477ea25b3a3a52abffdbcb16
Uni <joseoyun12@gmail.com>
2022-12-24 00:20:43 +0900

    글로벌 스타일 적용을 위해 추가함

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/GlobalStyle'
+import type { AppProps } from 'next/app';
+import GlobalStyle from '../styles/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle/>
-      <Component {...pageProps} />
-    </>
-  )
+	return (
+		<>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	);
 }
