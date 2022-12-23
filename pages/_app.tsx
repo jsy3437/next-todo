2147be90b270ac87d4b20c4ece46f2db4158f126
Uni <joseoyun12@gmail.com>
2022-12-24 00:21:34 +0900

    앱 공통 헤더 적용을 위해 추가함

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<GlobalStyle />
+			<Header />
 			<Component {...pageProps} />
 		</>
 	);
