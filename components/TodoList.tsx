2731ae54e647bb07524c1c898d9d0c52b8d40d16
Uni <joseoyun12@gmail.com>
2022-12-27 20:27:07 +0900

    페이지 내의 서버 데이터 패치를 집중할 수 있게 컴포넌트 새로 생성 후 추가함

---
+++ components/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	width: 100%;
+`;
+
+const TodoList: React.FC = () => {
+	return (
+		<Container>
+			<h1>TodoList</h1>
+		</Container>
+	);
+};
+export default TodoList;
