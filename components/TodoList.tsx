dd177aae7cb7225d9271dfa89686d6c6c649ab13
Uni <joseoyun12@gmail.com>
2022-12-29 05:01:59 +0900

    컴포넌트 props의 타입을 지정하고 todo데이터를 받아서 띄운뒤 기초 스타일링함

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from '../styles/palette';
+import { TodoType } from '../types/todo';
 
 const Container = styled.div`
 	width: 100%;
+
+	.todo-list-header {
+		padding: 12px;
+		border-bottom: 1px solid ${palette.gray};
+
+		.todo-list-last-todo {
+			font-size: 14px;
+			span {
+				margin-left: 8px;
+			}
+		}
+	}
 `;
 
-const TodoList: React.FC = () => {
+interface IProps {
+	todos: TodoType[];
+}
+
+const TodoList: React.FC<IProps> = ({ todos }) => {
 	return (
 		<Container>
-			<h1>TodoList</h1>
+			<div className="todo-list-header">
+				<p className="todo-list-last-todo">
+					남은 TODO<span>{todos.length}개</span>
+				</p>
+			</div>
 		</Container>
 	);
 };
