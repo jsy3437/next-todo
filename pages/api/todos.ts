7ee49a577a3f55e7ba174fdc364dd4c0cd86f99b
Uni <joseoyun12@gmail.com>
2023-01-03 10:20:46 +0900

    next API 사용을 위해 요청을 받았을때 응답해주는 코드 작성함

---
+++ pages/api/todos.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { TodoType } from '../../types/todo';
+import fs from 'fs';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method === 'GET') {
+		try {
+			const todosBuffer = fs.readFileSync('data/todos.json');
+			const todosString = todosBuffer.toString();
+			if (!todosString) {
+				res.statusCode = 200;
+				res.send([]);
+			}
+			const todos: TodoType[] = JSON.parse(todosString);
+			res.statusCode = 200;
+			return res.send(todos);
+		} catch (e) {
+			console.log(e);
+			res.statusCode = 405;
+			return res.end(e);
+		}
+	}
+};
