76e5127ea7dd8209423d4903d255eb19b9b0b3e9
Uni <joseoyun12@gmail.com>
2023-01-03 10:40:47 +0900

    next API를 이용하여 받은 데이터로 교체하는 코드 작성함

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,27 +4,21 @@ import TodoList from '../components/TodoList';
 import { TodoType } from '../types/todo';
 import { getTodosAPI } from '../lib/api/todo';
 
-const todos: TodoType[] = [
-	{ id: 1, text: '마트가서 장보기', color: 'red', checked: false },
-	{ id: 2, text: '수학 숙제하기', color: 'orange', checked: false },
-	{ id: 3, text: '코딩하기', color: 'yellow', checked: true },
-	{ id: 4, text: '넥스트 공부하기', color: 'green', checked: true },
-	{ id: 5, text: '요리 연습하기', color: 'blue', checked: false },
-	{ id: 6, text: '분리수거 하기', color: 'navy', checked: false },
-];
+interface IProps {
+	todos: TodoType[];
+}
 
-const app: NextPage = () => {
+const app: NextPage<IProps> = ({ todos }) => {
 	return <TodoList todos={todos} />;
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
 	try {
 		const { data } = await getTodosAPI();
-		console.log(data);
-		return { props: {} };
+		return { props: { todos: data } };
 	} catch (e) {
 		console.log(e);
-		return { props: {} };
+		return { props: { todos: [] } };
 	}
 };
 
