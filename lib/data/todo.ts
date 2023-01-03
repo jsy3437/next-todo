6a1531713698caaac8e15f4dab7e8f5be0d97b2f
Uni <joseoyun12@gmail.com>
2023-01-03 13:11:06 +0900

    todo 리스트를 가져오는 변수를 재사용 할 수있게 따로 만듬

--- lib/data/todo.ts
+++ lib/data/todo.ts
@@ -1,23 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { TodoType } from '../../types/todo';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method === 'GET') {
-		try {
-			const todosBuffer = readFileSync('data/todos.json');
-			const todosString = todosBuffer.toString();
-			if (!todosString) {
-				res.statusCode = 200;
-				res.send([]);
-			}
-			const todos: TodoType[] = JSON.parse(todosString);
-			res.statusCode = 200;
-			return res.send(todos);
-		} catch (e) {
-			console.log(e);
-			res.statusCode = 405;
-			return res.end(e);
-		}
+const getList = () => {
+	const todosBuffer = readFileSync('data/todos.json');
+	const todosString = todosBuffer.toString();
+	if (!todosString) {
+		return [];
 	}
+	const todos: TodoType[] = JSON.parse(todosString);
+	return todos;
 };
+
+export default { getList };
