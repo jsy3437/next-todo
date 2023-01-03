da250608fe432837f670eb04bb4c70cdbaae0637
Uni <joseoyun12@gmail.com>
2023-01-03 13:16:21 +0900

    todo 리스트를 가져오는 변수를 적용해 코드정리함

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -1,23 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { TodoType } from '../../../types/todo';
-import fs from 'fs';
+import Data from '../../../lib/data';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'GET') {
 		try {
-			const todosBuffer = fs.readFileSync('data/todos.json');
-			const todosString = todosBuffer.toString();
-			if (!todosString) {
-				res.statusCode = 200;
-				res.send([]);
-			}
-			const todos: TodoType[] = JSON.parse(todosString);
+			const todos = Data.todo.getList();
 			res.statusCode = 200;
 			return res.send(todos);
 		} catch (e) {
 			console.log(e);
-			res.statusCode = 405;
-			return res.end(e);
+			res.statusCode = 500;
+			return res.send(e);
 		}
 	}
 };
