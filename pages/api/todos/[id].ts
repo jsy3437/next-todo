20f94d5b73c1be03ebf75ef993800c1801e8924d
Uni <joseoyun12@gmail.com>
2023-01-03 13:29:51 +0900

    요청받은 id를 가진 데이터가 있는지 검사하는 코드 추가함

--- pages/api/todos/[id].ts
+++ pages/api/todos/[id].ts
@@ -1,7 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import Data from '../../../lib/data';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'PATCH') {
+		try {
+			const todoId = Number(req.query.id);
+			const todo = Data.todo.exist({ id: todoId });
+			if (!todo) {
+				res.statusCode = 404;
+				res.end();
+			}
+		} catch (e) {
+			console.log(e);
+			res.statusCode = 500;
+			res.send(e);
+		}
 	}
 	res.statusCode = 405;
 	return res.end();
