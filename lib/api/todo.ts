d265933b496e4377dda1b0cf8871e8cd10c89239
Uni <joseoyun12@gmail.com>
2023-01-03 13:33:13 +0900

    axios를 사용해 todo 체크상태 변경하는 요청변수를 추가함

--- lib/api/todo.ts
+++ lib/api/todo.ts
@@ -2,3 +2,4 @@ import axios from '.';
 import { TodoType } from '../../types/todo';
 
 export const getTodosAPI = () => axios.get<TodoType[]>('api/todos');
+export const checkTodoAPI = (id: number) => axios.patch(`api/todos/${id}`);
