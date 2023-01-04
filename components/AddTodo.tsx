dd53d7185016c31cb70dbab3d86c5db3244c7ade
Uni <joseoyun12@gmail.com>
2023-01-05 00:32:22 +0900

    todo 아이템 추가할 때 색깔 설정하는 버튼을 만들고 스타일링함

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import BrushIcon from '../public/statics/svg/brush.svg';
+import palette from '../styles/palette';
 
 const Container = styled.div`
 	padding: 16px;
@@ -22,6 +24,46 @@ const Container = styled.div`
 			font-size: 14px;
 		}
 	}
+
+	.add-todo-colors-wrapper {
+		width: 100%;
+		margin-top: 16px;
+		display: flex;
+		justify-content: space-between;
+
+		.add-todo-color-list {
+			display: flex;
+			button {
+				width: 24px;
+				height: 24px;
+				margin-right: 16px;
+				border: 0;
+				outline: 0;
+				border-radius: 50%;
+				&:last-child {
+					margin: 0;
+				}
+			}
+		}
+	}
+	.bg-blue {
+		background-color: ${palette.blue};
+	}
+	.bg-green {
+		background-color: ${palette.green};
+	}
+	.bg-navy {
+		background-color: ${palette.navy};
+	}
+	.bg-orange {
+		background-color: ${palette.orange};
+	}
+	.bg-red {
+		background-color: ${palette.red};
+	}
+	.bg-yellow {
+		background-color: ${palette.yellow};
+	}
 `;
 
 const AddTodo: React.FC = () => {
@@ -36,6 +78,21 @@ const AddTodo: React.FC = () => {
 				>
 					추가하기
 				</button>
+			</div>
+			<div className="add-todo-colors-wrapper">
+				<div className="add-todo-color-list">
+					{['red', 'orange', 'yellow', 'green', 'blue', 'navy'].map(
+						(color, index) => (
+							<button
+								key={index}
+								type="button"
+								className={`bg-${color} add-todo-color-button`}
+								onClick={() => {}}
+							/>
+						)
+					)}
+				</div>
+				<BrushIcon />
 			</div>
 		</Container>
 	);
