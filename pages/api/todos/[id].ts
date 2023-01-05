21bf2fab2d085fc35db6aea5ec3ebcf64889b9dc
Uni <joseoyun12@gmail.com>
2023-01-05 16:04:46 +0900

    todo 아이템 삭제요청 처리하는 코드 추가함

--- pages/api/todos/[id].ts
+++ pages/api/todos/[id].ts
@@ -26,6 +26,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			res.send(e);
 		}
 	}
+	if (req.method === 'DELETE') {
+		try {
+			const todoId = Number(req.query.id);
+			const todo = Data.todo.exist({ id: todoId });
+			if (!todo) {
+				res.statusCode = 404;
+				res.end();
+			}
+
+			const todos = Data.todo.getList();
+			const filteredTodos = todos.filter((todo) => todo.id !== todoId);
+			Data.todo.write(filteredTodos);
+			res.statusCode = 200;
+			res.end();
+		} catch (e) {
+			console.log(e);
+			res.statusCode = 500;
+			res.send(e);
+		}
+	}
 	res.statusCode = 405;
 	return res.end();
 };
