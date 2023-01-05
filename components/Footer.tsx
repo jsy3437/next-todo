ab4e6136aef74af678607947003c6bc6face8d17
Uni <joseoyun12@gmail.com>
2023-01-05 16:18:46 +0900

    하단에 todo 아이템 추가할 수 있는 푸터 컴포넌트 생성 후 스타일링함

---
+++ components/Footer.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import palette from '../styles/palette';
+
+const Container = styled.div`
+	width: 100%;
+	height: 53px;
+	position: fixed;
+	bottom: 0;
+	border-top: 1px solid ${palette.gray};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: white;
+	.footer-button {
+		font-size: 32px;
+		width: 32px;
+		height: 32px;
+		border-radius: 5px;
+		border: 1px solid black;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: white;
+		padding: 0;
+		line-height: 0;
+		outline: none;
+	}
+`;
+
+const Footer: React.FC = () => {
+	const router = useRouter();
+
+	return (
+		<Container>
+			<button
+				type="button"
+				className="footer-button"
+				onClick={() => router.push('/todo/add')}
+			>
+				+
+			</button>
+		</Container>
+	);
+};
+
+export default Footer;
