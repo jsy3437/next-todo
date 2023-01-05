e538d14b8df54e73f04f4d18e120792074f93298
Uni <joseoyun12@gmail.com>
2023-01-05 16:19:23 +0900

    푸터 컴포넌트를 모든페이지에서 볼 수 있게 추가함

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<GlobalStyle />
 			<Header />
 			<Component {...pageProps} />
+			<Footer />
 		</>
 	);
 }
