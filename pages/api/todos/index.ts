970f448ab6ce48c0ae68d0921ba6a5e975b38077
Uni <joseoyun12@gmail.com>
2023-01-05 15:18:02 +0900

    todo 아이템 추가하는 요청 처리 코드 추가함

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -13,4 +13,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			return res.send(e);
 		}
 	}
+	if (req.method === 'POST') {
+		const { text, color } = req.body;
+		if (!text || !color) {
+			res.statusCode = 400;
+			return res.send('text 혹은 color 가 없습니다.');
+		}
+		const todos = Data.todo.getList();
+		let todoId: number;
+		if (todos.length > 0) {
+			todoId = todos[todos.length - 1].id + 1;
+		} else {
+			todoId = 1;
+		}
+		const newTodo = {
+			id: todoId,
+			text,
+			color,
+			checked: false,
+		};
+
+		Data.todo.write([...todos, newTodo]);
+		res.statusCode = 200;
+		res.end();
+	}
 };
