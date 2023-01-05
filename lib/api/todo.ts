18d8d987aad929d23e7c9689bdb8372315323c6f
Uni <joseoyun12@gmail.com>
2023-01-05 15:16:28 +0900

    body 데이터 타입 선언하고 axios를 사용해 todo 아이템 추가하는 요청변수 추가함

--- lib/api/todo.ts
+++ lib/api/todo.ts
@@ -1,5 +1,12 @@
 import axios from '.';
 import { TodoType } from '../../types/todo';
 
+interface AddTodoAPIBody {
+	text: string;
+	color: TodoType['color'];
+}
+
 export const getTodosAPI = () => axios.get<TodoType[]>('api/todos');
 export const checkTodoAPI = (id: number) => axios.patch(`api/todos/${id}`);
+export const addTodoAPI = (body: AddTodoAPIBody) =>
+	axios.post('/api/todos', body);
