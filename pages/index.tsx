fe760bfbd0dd54c1d0c4d40d7571902655c818a0
Uni <joseoyun12@gmail.com>
2023-01-12 12:09:23 +0900

    store의 todos데이터를 사용하기 위해 props제거함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { NextPage } from 'next';
 import TodoList from '../components/TodoList';
-import { TodoType } from '../types/todo';
 import { getTodosAPI } from '../lib/api/todo';
 import { wrapper } from '../store';
 import { todoActions } from '../store/todo';
 
-interface IProps {
-	todos: TodoType[];
-}
-
-const app: NextPage<IProps> = ({ todos }) => {
-	return <TodoList todos={todos} />;
+const app: NextPage = () => {
+	return <TodoList />;
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
