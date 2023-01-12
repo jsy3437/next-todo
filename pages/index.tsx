0b8a4e666340e08428d0cb2c615056d2c94c5115
Uni <joseoyun12@gmail.com>
2023-01-12 11:57:50 +0900

    wrapper를 이용해 서버측 store를 받아오고 클라이언트에 업데이트해줌

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { GetServerSideProps, NextPage } from 'next';
+import { NextPage } from 'next';
 import TodoList from '../components/TodoList';
 import { TodoType } from '../types/todo';
 import { getTodosAPI } from '../lib/api/todo';
+import { wrapper } from '../store';
+import { todoActions } from '../store/todo';
 
 interface IProps {
 	todos: TodoType[];
@@ -12,14 +14,17 @@ const app: NextPage<IProps> = ({ todos }) => {
 	return <TodoList todos={todos} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-	try {
-		const { data } = await getTodosAPI();
-		return { props: { todos: data } };
-	} catch (e) {
-		console.log(e);
-		return { props: { todos: [] } };
+export const getServerSideProps = wrapper.getServerSideProps(
+	async ({ store }) => {
+		try {
+			const { data } = await getTodosAPI();
+			store.dispatch(todoActions.setTodo(data));
+			return { props: { todos: data } };
+		} catch (e) {
+			console.log(e);
+			return { props: { todos: [] } };
+		}
 	}
-};
+);
 
 export default app;
